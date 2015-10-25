23e04a6dc8bed148a1e2c531c0929982f6344f09
fravia1 <agnivo@gmail.com>
2015-10-26 05:07:47 +0530

    Update sbfrmsc-dti.sh

--- sbfrmsc-dti.sh
+++ sbfrmsc-dti.sh
@@ -297,7 +297,7 @@ apt-get --yes update
 apt-get --yes install whois sudo makepasswd git nano
 
 rm -f -r /etc/seedbox-from-scratch
-git clone -b v$SBFSCURRENTVERSION1 https://github.com/fravia1/seedbox-from-scratch.git /etc/seedbox-from-scratch
+git clone -b  https://github.com/fravia1/seedbox-from-scratch.git /etc/seedbox-from-scratch
 mkdir -p cd /etc/seedbox-from-scratch/source
 mkdir -p cd /etc/seedbox-from-scratch/users
 
